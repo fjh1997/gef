3306334c45c83ca4a3196e43f73b139590ee9fea
ValekoZ <lucas.valentin88@gmail.com>
2024-06-09 20:16:40 +0200

    Add type hints

--- gef.py
+++ gef.py
@@ -8859,8 +8859,8 @@ class VMMapCommand(GenericCommand):
 
         current_type: ArgType = ArgType.NONE
 
-        addrs = {}
-        names = []
+        addrs: Dict[str, int] = {}
+        names: List[str] = []
 
         for arg in argv:
             if arg in ("-a", "--addr"):
